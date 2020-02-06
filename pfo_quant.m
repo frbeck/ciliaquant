2f8a4cbfecbac6fe503cc0c7a6d9f77982976086
frbeck <53025071+frbeck@users.noreply.github.com>
2020-02-06 15:24:39 -0800

    Add files via upload

--- pfo_quant.m
+++ pfo_quant.m
@@ -1,37 +1,46 @@
+%Make an array to hold pixel intensity values
 k = [];
-n = 20;
+%Indicate the sample size
+n = 2;
+%Make a matrix to holld y length values
+ylTable = [];
+%For each image identify the y length and insert it into the array ylTable
 for i = 1:n
     T = readtable(strcat(num2str(i), '.csv'));
     A = table2array(T);
-    x = A(2:1500, 1);
-    y = A(2:1500, 2);
-    yy = smoothdata(y, 'gaussian'); 
+    sz = size(A);
+    sz2 = sz(1);
+    ylTable = [ylTable, sz2];
+end
+%Select the smallest y length from ylTable
+ymin = min(ylTable);
+%Compile the data and normalize against the noise
+for i = 1:n
+    T = readtable(strcat(num2str(i), '.csv'));
+    A = table2array(T);
+    x = A(2:ymin, 1);
+    y = A(2:ymin, 3);
+    y1 = A(1, 3);
+    Nr =  y./y1; 
     s = num2str(i);
-    f = yy;
+    f = Nr;
+    %insert the normalized data into a new matrix
     k = [k,f];
     disp(i)
-    plot (f)
     hold on
 end
+%Average the all of the data arrays
 M = mean(k,2);
+smoothy = smoothdata(M, 'rloess');
+%Smooth the averaged data
 smoothM = smoothdata(M, 'gaussian');
-disp(smoothM)
-plot(smoothM);
-hold off
-plot(smoothM);
+%Plot the results
+derivM = diff(smoothM);
+scatter(x, smoothM)
 hold on
-p = poly(M);
-
-%sz = size(M);
-%disp(sz)
-%xn = linspace(0,2,3999);
-%xx = xn.';
-%p = polyfit(xx,M,100);
-%plot (p)
+plot(M)
+%plot(f)
 hold off
-
-
-
 
 
 
